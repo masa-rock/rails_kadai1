a365a0a8a0c28899bf017b172187acc3b9a6f41e
masa-rock <79382412+masa-rock@users.noreply.github.com>
2022-05-01 21:33:03 +0900

    railsージョン切り替え

---
+++ db/migrate/20220430052222_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.datetime :start_date
+      t.datetime :end_date
+      t.boolean :all_day
+
+      t.timestamps
+    end
+  end
+end
